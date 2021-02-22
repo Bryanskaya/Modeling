511b1d5d3991dec642f5e874dfb13c6cb32cccd9
Bryanskaya <bryanskayakatya@yandex.ru>
2021-02-22 16:28:51 +0300

    ADD: last method

--- lab_01/program/main.py
+++ lab_01/program/main.py
@@ -7,22 +7,43 @@ def f(x, y):
 
 
 # Метод Пикара
-def picard_1(x):
-    return x ** 3 / 3
+def picard_1(x_args):
+    res = []
+
+    for x in x_args:
+        res.append(x ** 3 / 3)
+
+    return res
 
 
-def picard_2(x):
-    return picard_1(x) + x ** 7 / 63
+def picard_2(x_args):
+    res = []
+
+    for x in x_args:
+        res.append(x ** 3 / 3 + x ** 7 / 63)
+
+    return res
 
 
-def picard_3(x):
-    return picard_2(x) + x ** 15 / 59535 + 2 * x ** 11 / 2079
+def picard_3(x_args):
+    res = []
+
+    for x in x_args:
+        res.append(x ** 3 / 3 + x ** 7 / 63 + x ** 15 / 59535 + 2 * x ** 11 / 2079)
+
+    return res
 
 
-def picard_4(x):
-    return picard_3(x) + x ** 31 / 109876902975 + 4 * x ** 23 / 99411543 + \
-           4 * x ** 27 / 3341878155 + 2 * x ** 23 / 86266215 + \
-           2 * x ** 19 / 3393495 + 4 * x ** 19 / 2488563 + 4 * x ** 15 / 93555
+def picard_4(x_args):
+    res = []
+
+    for x in x_args:
+        res.append(x ** 3 / 3 + x ** 7 / 63 + x ** 15 / 59535 + 2 * x ** 11 / 2079 + \
+                   x ** 31 / 109876902975 + 4 * x ** 23 / 99411543 + \
+                   4 * x ** 27 / 3341878155 + 2 * x ** 23 / 86266215 + \
+                   2 * x ** 19 / 3393495 + 4 * x ** 19 / 2488563 + 4 * x ** 15 / 93555)
+
+    return res
 
 
 def runge_kutta(x, y, h, num):
@@ -32,7 +53,7 @@ def runge_kutta(x, y, h, num):
     temp = h / (2 * alpha)
 
     for i in range(num):
-        res.append([x, y])
+        res.append(y)
 
         k1 = f(x, y)
         k2 = f(x + temp, y + temp * k1)
@@ -47,14 +68,14 @@ def euler_explicit(x, y, h, num):
     res = []
 
     for i in range(num):
-        res.append([x, y])
+        res.append(y)
 
         try:
             y += h * f(x, y)
             x += h
         except OverFlowError:
             for k in range(i, num):
-                res.append([x, '---'])
+                res.append('---')
             break
 
     return res
@@ -64,54 +85,81 @@ def euler_implicit(x, y, h, num):
     res = []
 
     for i in range(num):
-        res.append([x, y])
+        res.append(y)
 
         x += h
         D = 1 - 4 * h * y - 4 * h ** 2 * x ** 2
         if D < 0:
             for k in range(i, n):
-                res.append([x, '---'])
+                res.append('---')
             break
-        y = (1 - sqrt(D)) / (2 * h)   # +sqrt(D)
+        y = (1 - sqrt(D)) / (2 * h)
 
     return res
 
 
+def count_x_args(x, x_max, h):
+    x_args = []
+
+    while x <= x_max:
+        x_args.append(x)
+        x += h
+
+    return x_args
     
-
-
 
 def print_head():
     print(' ' * 4 + 'x' + ' ' * 4 + '|' + \
-          ' ' * 9 + 'Метод Пикара' + ' ' * 10 + '|' + \
-          ' ' * 3 + 'Метод Эйлера' + ' ' * 3 + '|' + \
+          ' ' * 17 + 'Метод Пикара' + ' ' * 18 + '|' + \
+          ' ' * 6 + 'Метод Эйлера' + ' ' * 5 + '|' + \
           ' ' * 3 + 'Метод Рунге-Кутта' + ' ' * 3 +
           '\n' + \
           ' ' * 9 + '|' + \
-          ' ' * 3 + '1' + ' ' * 3 + '|' + \
-          ' ' * 3 + '2' + ' ' * 3 + '|' + \
-          ' ' * 3 + '3' + ' ' * 3 + '|' + \
-          ' ' * 3 + '4' + ' ' * 3 + '|' + \
-          ' ' * 18 + '|' + \
-          '\n' + '̅ ' * 50)
+          ' ' * 5 + '1' + ' ' * 5 + '|' + \
+          ' ' * 5 + '2' + ' ' * 5 + '|' + \
+          ' ' * 5 + '3' + ' ' * 5 + '|' + \
+          ' ' * 5 + '4' + ' ' * 5 + '|' + \
+          ' ' * 3 + 'Явный' + ' ' * 3 + '|' + \
+          ' ' * 2 + 'Неявный' + ' ' * 2 + '|' + \
+          '\n' + '̅ ' * 55)
 
 
 def main():
     print_head()
 
-    x, y = 0, 0
-    h = 0.1
+    x, x_max, y = 0, 2, 0
+    h = 10 ** -6 / 2
 
-    print('Кутт', runge_kutta(x, y, h, 10))
-    print('Явный Эйлер ', euler_explicit(x, y, h, 10))
-    print('Мнимый Эйлер', euler_implicit(x, y, h, 10))
+    num = int((x_max - x) / h)
+    x_args = count_x_args(x, x_max, h)
 
-    for x in range(0, 10):
-        print('{:9.3f}|{:7.3f}|{:7.3f}|{:7.3f}|{:7.3f}'.format(x,
-                                                               picard_1(x),
-                                                               picard_2(x),
-                                                               picard_3(x),
-                                                               picard_4(x)))
+    res_runge_kutta = runge_kutta(x, y, h, num)
+    res_euler_explicit = euler_explicit(x, y, h, num)
+    res_euler_implicit = euler_implicit(x, y, h, num)
+    res_picard_1 = picard_1(x_args)
+    res_picard_2 = picard_2(x_args)
+    res_picard_3 = picard_3(x_args)
+    res_picard_4 = picard_4(x_args)
+
+    #f = open('results.csv', 'w')
+    for i in range(num):
+        if not i % 1000:
+            print('{:9.3e}|{:11.3e}|{:11.3e}|{:11.3e}|{:11.3e}|{:11.3e}|{:11.3e}|{:11.3e}'.format(x_args[i],
+                                                                                              res_picard_1[i],
+                                                                                              res_picard_2[i],
+                                                                                              res_picard_3[i],
+                                                                                              res_picard_4[i],
+                                                                                              res_euler_explicit[i],
+                                                                                              res_euler_implicit[i],
+                                                                                              res_runge_kutta[i]))
+            '''f.write('{:9.3e},{:11.3e},{:11.3e},{:11.3e},{:11.3e},{:11.3e},{:11.3e},{:11.3e}\n'.format(x_args[i],
+                                                                                                  res_picard_1[i],
+                                                                                                  res_picard_2[i],
+                                                                                                  res_picard_3[i],
+                                                                                                  res_picard_4[i],
+                                                                                                  res_euler_explicit[i],
+                                                                                                  res_euler_implicit[i],
+                                                                                                  res_runge_kutta[i]))'''
 
 
 if __name__ == '__main__':
